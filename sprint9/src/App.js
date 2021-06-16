96b6bca0033d5bef2190d984ebd4802308151ecf
Francesc <fbedos@hotmail.com>
2021-06-16 12:44:39 +0200

    construint APP

--- sprint9/src/App.js
+++ sprint9/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import SideBar from './components/SideBar/SideBar';
+import RecommendedVideos from './components/RecommendedVideos/RecommendedVideos';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
+
   return (
     <div className="App">
-     
+      <Router>
+      <Header />
+        <Switch>
+          <Route path='/'>
+            <div className="app__mainpage">
+              <SideBar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
